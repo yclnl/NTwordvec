e9c0287deb2d02bed6e7cec5e7c241a6dc137c7d
林奕辰 <yclnl@linyichendeMacBook-Pro.local>
2017-03-20 15:51:18 +0800

    fix word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -1,8 +1,11 @@
 import fasttext
 import numpy as np
 import json
+from sklearn.tree import ExtraTreeClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.model_selection import train_test_split
 model = fasttext.cbow('rawData.txt', 'model')
-ngram = 2
+ngrams = 3
 id_map = json.load(open('label_id.json'))
 count = 0
 feature = []
@@ -13,20 +16,21 @@ for line in open('data_seg.txt'):
     label_id = id_map[label][0]
     target.append([label_id])
     wordList = lineList[:len(lineList)-1]
-    wordvec = np.zeros((100))
-    if len(wordList)>ngram:
-        for i in range(0,len(wordList)-ngram):
-            word=''
-            for idx in range(i,i+ngram-1):
-                word+=wordList[i]
-            wordvec += np.array(model[word])
+    wordvec = np.zeros(100*ngram)
+    for ngram in range(1,ngrams+1):
+        if len(wordList)>=ngram:
+            for i in range(0,len(wordList)-ngram):
+                word=''
+                for idx in range(i,i+ngram):
+                    word+=wordList[idx]
+                wordvec[100*(ngram-1):100*(ngram)] += np.array(model[word])
 
-        wordvec /= (len(wordList)-ngram+1)
-    else:
-        word = ''
-        for subword in wordList:
-            word+=subword
-        wordvec = np.array(model[word])
+            wordvec /= (len(wordList)-ngram+1)
+        else:
+            word = ''
+            for subword in wordList:
+                word+=subword
+            wordvec[100*(ngram-1):100*ngram] = np.array(model[word])
     feature.append(wordvec.tolist())
 feature =np.array(feature)
 print(feature.shape)
